68f3321cdf480038650be25015c9175a5a13ade5
nakajimanaoto2014 <homurann1996@gmail.com>
2025-05-25 22:12:08 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -4,10 +4,14 @@ let bodyWeight = 68;
 //変数の宣言
 let  height = 1.7;
 
+//変数の宣言
+let bmi = bodyWeight / (height * height);
+
 //コンソールへの出力
 console.log(bodyWeight);
 
 //コンソールへの出力
 console.log(height);
 
-console.log(bodyWeight /= height *= height);
+//コンソールへの出力
+console.log(bmi);
