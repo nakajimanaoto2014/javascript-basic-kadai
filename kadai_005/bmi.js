a423b8c1e17b817ec49ed06a897ee16bf29b5960
nakajimanaoto2014 <homurann1996@gmail.com>
2025-05-25 22:06:04 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+//変数の宣言
+let bodyWeight = 68;
+
+//変数の宣言
+let  height = 1.7;
+
+//コンソールへの出力
+console.log(bodyWeight);
+
+//コンソールへの出力
+console.log(height);
+
+console.log(bodyWeight /= height *= height);
