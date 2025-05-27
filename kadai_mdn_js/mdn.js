1ad9cc5ad4e8cc1023e73e280f8a69895454cb02
nakajimanaoto2014 <homurann1996@gmail.com>
2025-05-28 00:40:23 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,4 +1,12 @@
-const Kadai  = () => {
- console.log("2025年5月27日");
+const date = new Date();
+const options = {
+  year:'numeric',
+  month:'long',
+  day:'numeric',
 }
-Kadai();
+
+const year = date.getFullYear(); // 現在の年を取得
+const month = date.getMonth() + 1; // 現在の月を取得（0から11で表されるため、+1する）
+const day = date.getDate(); // 現在の日を取得
+
+console.log(`${year}年${month}月${day}日`);
